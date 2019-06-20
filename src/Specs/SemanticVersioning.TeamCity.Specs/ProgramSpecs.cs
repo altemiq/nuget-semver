56f1bc238d5aa63a54ab9079aa1d173e9389ca05
Ross King <ross.king@mondo.com.au>
2019-06-20 13:52:34 +1000

    Added in capturing StdErr as well as StdOut

--- src/Specs/SemanticVersioning.TeamCity.Specs/ProgramSpecs.cs
+++ src/Specs/SemanticVersioning.TeamCity.Specs/ProgramSpecs.cs
@@ -9,28 +9,36 @@ namespace Mondo.SemanticVersioning.TeamCity
 
         protected static System.Reflection.MethodInfo MainMethod;
 
-        protected static (int, System.Collections.Generic.IEnumerable<string>) Invoke(params string[] args)
+        protected static (int, System.Collections.Generic.IEnumerable<string>, System.Collections.Generic.IEnumerable<string>) Invoke(params string[] args)
         {
-            var consoleWriter = new ConsoleWriter(System.Console.Out);
-            System.Console.SetOut(consoleWriter);
+            var consoleOut = new ConsoleWriter(System.Console.Out);
+            System.Console.SetOut(consoleOut);
+
+            var consoleError = new ConsoleWriter(System.Console.Error);
+            System.Console.SetError(consoleError);
+
             var returnObject = MainMethod.Invoke(null, new object[] { args });
 
-            (int, System.Collections.Generic.IEnumerable<string>) returnValue;
+            (int, System.Collections.Generic.IEnumerable<string>, System.Collections.Generic.IEnumerable<string>) returnValue;
             switch (returnObject)
             {
                 case System.Threading.Tasks.Task<int> intTask:
-                    returnValue = (intTask.Result, consoleWriter.CapturedOutput);
+                    returnValue = (intTask.Result, consoleOut.CapturedOutput, consoleError.CapturedOutput);
                     break;
                 case System.Threading.Tasks.Task task:
                     task.Wait();
-                    returnValue = (0, consoleWriter.CapturedOutput);
+                    returnValue = (0, consoleOut.CapturedOutput, consoleError.CapturedOutput);
                     break;
                 case int intValue:
-                    returnValue = (intValue, consoleWriter.CapturedOutput);
+                    returnValue = (intValue, consoleOut.CapturedOutput, consoleError.CapturedOutput);
                     break;
                 default:
-                    return (0, consoleWriter.CapturedOutput);
+                    returnValue = (0, consoleOut.CapturedOutput, consoleError.CapturedOutput);
+                    break;
             }
+
+            System.Console.SetOut(consoleOut.Original);
+            System.Console.SetError(consoleError.Original);
 
             return returnValue;
         }
@@ -67,32 +75,26 @@ namespace Mondo.SemanticVersioning.TeamCity
         {
             private readonly System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder();
 
-            public ConsoleWriter(System.IO.TextWriter consoleOut) => this.Out = consoleOut;
+            public ConsoleWriter(System.IO.TextWriter original) => this.Original = original;
 
-            public System.Collections.Generic.IEnumerable<string> CapturedOutput => this.stringBuilder.ToString().Split(System.Environment.NewLine);
+            public System.Collections.Generic.IEnumerable<string> CapturedOutput => this.stringBuilder.Length == 0
+                ? System.Linq.Enumerable.Empty<string>()
+                : this.stringBuilder.ToString().Split(System.Environment.NewLine);
 
-            private readonly System.IO.TextWriter Out;
+            public System.IO.TextWriter Original { get; }
 
             public override System.Text.Encoding Encoding => System.Text.Encoding.UTF8;
 
-            public override void Write(string value)
-            {
-                this.stringBuilder.Append(value);
-                this.Out.Write(value);
-            }
+            public override void Write(string value) => this.stringBuilder.Append(value);
 
-            public override void WriteLine(string value)
-            {
-                this.stringBuilder.AppendLine(value);
-                this.Out.WriteLine(value);
-            }
+            public override void WriteLine(string value) => this.stringBuilder.AppendLine(value);
         }
     }
 
     [Subject(typeof(Program))]
     internal class When_running_the_program_with_no_valid_nuget : When_running_the_program
     {
-        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console) returnValue;
+        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console, System.Collections.Generic.IEnumerable<string> error) returnValue;
 
         private readonly Because of = () => returnValue = Invoke("diff", "solution", GetProjectPath("Projects\\Original"), "--source", GetSource("NoPackages"), "--no-cache");
 
@@ -101,12 +103,14 @@ namespace Mondo.SemanticVersioning.TeamCity
         private readonly It should_have_returned_a_version = () => returnValue.console.Should().Contain("##teamcity[system.build.number '0.1.0']");
 
         private readonly It should_have_returned_a_blank_suffix = () => returnValue.console.Should().Contain("##teamcity[system.build.suffix '']");
+
+        private readonly It should_not_have_thrown_an_exception = () => returnValue.error.Should().BeEmpty();
     }
 
     [Subject(typeof(Program))]
     internal class When_running_the_program_with_no_full_release : When_running_the_program
     {
-        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console) returnValue;
+        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console, System.Collections.Generic.IEnumerable<string> error) returnValue;
 
         private readonly Because of = () => returnValue = Invoke("diff", "solution", GetProjectPath("Projects\\Original"), "--source", GetSource("OnlyPrerelease"), "--no-cache");
 
@@ -115,12 +119,14 @@ namespace Mondo.SemanticVersioning.TeamCity
         private readonly It should_have_returned_a_version = () => returnValue.console.Should().Contain("##teamcity[system.build.number '1.0.2']");
 
         private readonly It should_have_returned_a_suffix = () => returnValue.console.Should().Contain("##teamcity[system.build.suffix 'develop']");
+
+        private readonly It should_not_have_thrown_an_exception = () => returnValue.error.Should().BeEmpty();
     }
 
     [Subject(typeof(Program))]
     internal class When_running_the_program_with_no_pre_release : When_running_the_program
     {
-        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console) returnValue;
+        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console, System.Collections.Generic.IEnumerable<string> error) returnValue;
 
         private readonly Because of = () => returnValue = Invoke("diff", "solution", GetProjectPath("Projects\\Original"), "--source", GetSource("OnlyRelease"), "--direct-download", "--no-cache");
 
@@ -129,12 +135,14 @@ namespace Mondo.SemanticVersioning.TeamCity
         private readonly It should_have_returned_a_version = () => returnValue.console.Should().Contain("##teamcity[system.build.number '1.0.1']");
 
         private readonly It should_have_returned_a_blank_suffix = () => returnValue.console.Should().Contain("##teamcity[system.build.suffix '']");
+
+        private readonly It should_not_have_thrown_an_exception = () => returnValue.error.Should().BeEmpty();
     }
 
     [Subject(typeof(Program))]
     internal class When_running_the_program_with_pre_release_and_release : When_running_the_program
     {
-        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console) returnValue;
+        private static (int exitValue, System.Collections.Generic.IEnumerable<string> console, System.Collections.Generic.IEnumerable<string> error) returnValue;
 
         private readonly Because of = () => returnValue = Invoke("diff", "solution", GetProjectPath("Projects\\Original"), "--source", GetSource("Full"), "--direct-download", "--no-cache");
 
@@ -143,5 +151,7 @@ namespace Mondo.SemanticVersioning.TeamCity
         private readonly It should_have_returned_a_version = () => returnValue.console.Should().Contain("##teamcity[system.build.number '1.0.2']");
 
         private readonly It should_have_returned_a_blank_suffix = () => returnValue.console.Should().Contain("##teamcity[system.build.suffix 'develop']");
+
+        private readonly It should_not_have_thrown_an_exception = () => returnValue.error.Should().BeEmpty();
     }
 }
