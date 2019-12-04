c3198c5183c4cd84dd33b08f740e6cc9c65b30c4
Ross King <ross.king@mondo.com.au>
2019-12-04 13:00:48 +1100

    Added more target frameworks for the build

--- docker_build.cmd
+++ docker_build.cmd
@@ -1,4 +1,4 @@
 SET version=1.0.48
 
-docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:3.0 /build/test.sh %version%
-docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:3.0 /build/pack.sh %version%
+docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:latest /build/test.sh %version%
+docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:latest /build/pack.sh %version%
