a77d5ff31396923cb2d5e2cd7dbceef39d77493f
Ross King <ross.king@ausnetservices.com.au>
2021-01-06 21:16:31 +1100

    Updated docker build script to use .NET 5.0

--- docker_build.cmd
+++ docker_build.cmd
@@ -1,4 +1,6 @@
 SET version=1.0.72
+SET tag=latest
 
-docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:latest /build/test.sh %version%
-docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/core/sdk:latest /build/pack.sh %version%
+docker pull mcr.microsoft.com/dotnet/sdk:%tag%
+docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/sdk:%tag% /build/test.sh %version%
+docker run --rm --workdir /build --volume %~dp0:/build --volume %USERPROFILE%\.nuget\packages:/root/.nuget/packages --entrypoint /bin/sh mcr.microsoft.com/dotnet/sdk:%tag% /build/pack.sh %version%
