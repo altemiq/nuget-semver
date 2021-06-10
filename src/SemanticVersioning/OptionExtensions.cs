cfe8554c097dfe9c5e69abc1441ec5665d8b6bdf
Ross King <ross.king@ausnetservices.com.au>
2021-06-11 09:12:22 +1000

    refactor: move to top-level program

---
+++ src/SemanticVersioning/OptionExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Altemiq.SemanticVersioning
+{
+    using System.CommandLine;
+
+    internal static class OptionExtensions
+    {
+
+        public static Option<T> WithArgumentName<T>(this Option<T> option, string name)
+        {
+            option.Argument.Name = name;
+            return option;
+        }
+
+        public static Option<T> WithDefaultValue<T>(this Option<T> option, T value)
+        {
+            option.Argument.SetDefaultValue(value);
+            return option;
+        }
+
+        public static Option<T> WithArity<T>(this Option<T> option, IArgumentArity arity)
+        {
+            option.Argument.Arity = arity;
+            return option;
+        }
+    }
+}
