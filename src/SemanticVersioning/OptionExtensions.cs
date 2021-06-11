bb7949132d45603b39355b244277bddb5f64dc10
Ross King <ross.king@ausnetservices.com.au>
2021-06-11 11:58:47 +1000

    feat: update to latest command-line library

--- src/SemanticVersioning/OptionExtensions.cs
+++ src/SemanticVersioning/OptionExtensions.cs
@@ -6,49 +6,22 @@
 
 namespace Mondo.SemanticVersioning
 {
-    using System.CommandLine;
-
     /// <summary>
-    /// The <see cref="Option{T}"/> extensions.
+    /// The <see cref="System.CommandLine.Option{T}"/> extensions.
     /// </summary>
     internal static class OptionExtensions
     {
         /// <summary>
-        /// Sets the argument name.
+        /// Adds the alias.
         /// </summary>
         /// <typeparam name="T">The type of argument.</typeparam>
         /// <param name="option">The option.</param>
-        /// <param name="name">The argument name.</param>
+        /// <param name="alias">The alias.</param>
         /// <returns>The option to chain.</returns>
-        public static Option<T> WithArgumentName<T>(this Option<T> option, string name)
+        public static T WithAlias<T>(this T option, string alias)
+            where T : System.CommandLine.Option
         {
-            option.Argument.Name = name;
-            return option;
-        }
-
-        /// <summary>
-        /// Sets the default value.
-        /// </summary>
-        /// <typeparam name="T">The type of argument.</typeparam>
-        /// <param name="option">The option.</param>
-        /// <param name="value">The default value.</param>
-        /// <returns>The option to chain.</returns>
-        public static Option<T> WithDefaultValue<T>(this Option<T> option, T value)
-        {
-            option.Argument.SetDefaultValue(value);
-            return option;
-        }
-
-        /// <summary>
-        /// Sets the arity.
-        /// </summary>
-        /// <typeparam name="T">The type of argument.</typeparam>
-        /// <param name="option">The option.</param>
-        /// <param name="arity">The arity.</param>
-        /// <returns>The option to chain.</returns>
-        public static Option<T> WithArity<T>(this Option<T> option, IArgumentArity arity)
-        {
-            option.Argument.Arity = arity;
+            option.AddAlias(alias);
             return option;
         }
     }
