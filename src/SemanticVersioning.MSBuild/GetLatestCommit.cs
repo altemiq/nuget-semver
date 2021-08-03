94d2a63878b1c92b3c38ff1f0c6f47a549bbf804
Ross King <ross.king@ausnetservices.com.au>
2021-08-03 10:09:43 +1000

    feat: use reference commit to determine version
    if any reference has a newer commit that the project, then the project needs a new version

---
+++ src/SemanticVersioning.MSBuild/GetLatestCommit.cs
@@ -0,0 +1,72 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetLatestCommit.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altemiq.SemanticVersioning
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Build.Framework;
+
+    /// <summary>
+    /// Gets the latest commit for a set of paths.
+    /// </summary>
+    public class GetLatestCommit : Microsoft.Build.Utilities.Task
+    {
+        private readonly GetProjectCommits task = new();
+
+        /// <summary>
+        /// Gets or sets the project dir.
+        /// </summary>
+        [Required]
+        public ITaskItem[] Paths { get; set; } = default!;
+
+        /// <summary>
+        /// Gets the commits.
+        /// </summary>
+        [Output]
+        public string? Commit { get; private set; }
+
+        /// <inheritdoc cref="Microsoft.Build.Utilities.ToolTask.ToolPath" />
+        public string ToolPath
+        {
+            get => this.task.ToolPath;
+            set => this.task.ToolPath = value;
+        }
+
+        /// <inheritdoc cref="Microsoft.Build.Utilities.ToolTask.ToolExe" />
+        public string ToolExe
+        {
+            get => this.task.ToolExe;
+            set => this.task.ToolExe = value;
+        }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            this.task.BuildEngine = this.BuildEngine;
+            GitCommit? commit = default;
+
+            foreach (var path in this.Paths)
+            {
+                this.task.GitCommits.Clear();
+                this.task.ProjectDir = path.ItemSpec;
+                if (this.task.Execute())
+                {
+                    var currentCommit = this.task.GitCommits.FirstOrDefault();
+                    if (commit is null || (currentCommit is not null && commit.AuthorDate < currentCommit.AuthorDate))
+                    {
+                        commit = currentCommit;
+                    }
+                }
+            }
+
+            this.Commit = commit?.Sha;
+            return this.Commit is not null;
+        }
+    }
+}
---
+++ src/SemanticVersioning.MSBuild/GitTask.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GitTask.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altemiq.SemanticVersioning
+{
+    /// <summary>
+    /// The base GIT task.
+    /// </summary>
+    public abstract class GitTask : Microsoft.Build.Utilities.ToolTask
+    {
+        /// <inheritdoc/>
+        protected override string ToolName => "git";
+
+        /// <summary>
+        /// Gets the base GIT directory.
+        /// </summary>
+        /// <param name="directory">The directory.</param>
+        /// <returns>The base GIT directory.</returns>
+        protected static string? GetBaseDirectory(string? directory)
+        {
+            while (directory is not null)
+            {
+                var git = System.IO.Path.Combine(directory, ".git");
+                if (System.IO.Directory.Exists(git))
+                {
+                    return directory;
+                }
+
+                directory = System.IO.Path.GetDirectoryName(directory);
+            }
+
+            return default;
+        }
+
+        /// <inheritdoc/>
+        protected override string GenerateFullPathToTool()
+        {
+            if (this.ToolPath is null)
+            {
+                return this.ToolExe;
+            }
+
+            return System.IO.Path.Combine(this.ToolPath, this.ToolExe);
+        }
+    }
+}
