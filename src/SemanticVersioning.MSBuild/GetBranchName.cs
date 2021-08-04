f3d73ec9b1ddae095d43ccf4ba88d12789d821a8
Ross King <ross.king@ausnetservices.com.au>
2021-08-04 14:57:29 +1000

    feat: add task to get the branch name

---
+++ src/SemanticVersioning.MSBuild/GetBranchName.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetBranchName.cs" company="Mondo">
+// Copyright (c) Mondo. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Mondo.SemanticVersioning
+{
+    /// <summary>
+    /// Get the branch name.
+    /// </summary>
+    public sealed class GetBranchName : GitTask
+    {
+        /// <summary>
+        /// Gets or sets the project dir.
+        /// </summary>
+        [Microsoft.Build.Framework.Required]
+        public string ProjectDir { get; set; } = default!;
+
+        /// <summary>
+        /// Gets the latest commit.
+        /// </summary>
+        [Microsoft.Build.Framework.Output]
+        public string? Branch { get; private set; }
+
+        /// <inheritdoc/>
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new Microsoft.Build.Utilities.CommandLineBuilder();
+            builder.AppendSwitch("branch");
+            builder.AppendSwitch("--show-current");
+            return builder.ToString();
+        }
+
+        /// <inheritdoc/>
+        protected override string? GetWorkingDirectory() => GetBaseDirectory(this.ProjectDir);
+
+        /// <inheritdoc/>
+        protected override void LogEventsFromTextOutput(string singleLine, Microsoft.Build.Framework.MessageImportance messageImportance)
+        {
+            this.Branch = singleLine;
+            base.LogEventsFromTextOutput(singleLine, messageImportance);
+        }
+    }
+}
