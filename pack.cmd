a0b0c6e8d734767e69fcd4b770104db6e3833536
Ross King <ross.king@ausnetservices.com.au>
2021-08-06 15:32:16 +1000

    build: removed TeamCity zip

--- pack.cmd
+++ pack.cmd
@@ -11,22 +11,3 @@ REM MSBuild Tasks
 dotnet pack ^
  .\src\SemanticVersioning.MSBuild\SemanticVersioning.MSBuild.csproj ^
  --output .\nupkg
-
-SET TargetFramework=netcoreapp3.1
-
-REM TeamCity Tool
-MKDIR pack
-CD pack
-MKDIR win-x64
-MKDIR linux-x64
-
-COPY ..\src\SemanticVersioning.Commandline\bin\%Configuration%\%TargetFramework%\win-x64\publish\* win-x64\
-COPY ..\src\SemanticVersioning.Commandline\bin\%Configuration%\%TargetFramework%\linux-x64\publish\* linux-x64\
-COPY ..\src\teamcity-plugin.xml
-..\7za.exe a ..\SemanticVersioning.DEFAULT.zip
-
-CD ..
-
-COPY SemanticVersioning.DEFAULT.zip SemanticVersioning.%1.zip
-
-RMDIR pack /S /Q
