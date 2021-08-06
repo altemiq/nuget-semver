f9921fb769ddf85fc33f63eb51f646cb3faea90a
Ross King <ross.king@ausnetservices.com.au>
2021-08-06 13:18:19 +1000

    build: update build script

--- publish.cmd
+++ publish.cmd
@@ -1,15 +1,18 @@
+SET TargetFramework=netcoreapp3.1
+
 dotnet publish ^
- src\SemanticVersioning\SemanticVersioning.csproj ^
+ src\SemanticVersioning.CommandLine\SemanticVersioning.CommandLine.csproj ^
  --configuration Release ^
+ --framework %TargetFramework% ^
  --runtime win-x64 ^
  -property:Version=%1 ^
  -property:PublishSingleFile=true ^
  -property:PublishTrimmed=true ^
  -property:ContinuousIntegration=true
 dotnet publish ^
- src\SemanticVersioning\SemanticVersioning.csproj ^
+ src\SemanticVersioning.CommandLine\SemanticVersioning.CommandLine.csproj ^
  --configuration Release ^
- --runtime linux-x64 ^
+ --runtime %TargetFramework% ^
  -property:Version=%1 ^
  -property:PublishSingleFile=true ^
  -property:PublishTrimmed=true ^
