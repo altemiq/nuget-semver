55c527dcd0c9e87f6153771e608840b323f162b1
Ross King <ross.king@ausnetservices.com.au>
2021-08-12 21:00:37 +1000

    feat: read/write version to a file
    this short cuts computations for the same commit for a project
    this should also speed up any computations for multiple runs in a build

---
+++ src/SemanticVersioning.MSBuild/ReadVersionFromFile.cs
@@ -0,0 +1,71 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ReadVersionFromFile.cs" company="Mondo">
+// Copyright (c) Mondo. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Mondo.SemanticVersioning
+{
+    using Microsoft.Build.Framework;
+
+    /// <summary>
+    /// Reads the version from the file.
+    /// </summary>
+    public sealed class ReadVersionFromFile : Microsoft.Build.Tasks.ReadLinesFromFile
+    {
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        [Output]
+        public string? Version { get; private set; }
+
+        /// <summary>
+        /// Gets the version prefix.
+        /// </summary>
+        [Output]
+        public string? VersionPrefix { get; private set; }
+
+        /// <summary>
+        /// Gets the version suffix.
+        /// </summary>
+        [Output]
+        public string? VersionSuffix { get; private set; }
+
+        /// <summary>
+        /// Gets the repository commit.
+        /// </summary>
+        [Output]
+        public string? RepositoryCommit { get; private set; }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            if (base.Execute())
+            {
+                if (this.Lines.Length > 0)
+                {
+                    this.Version = this.Lines[0].ItemSpec;
+                }
+
+                if (this.Lines.Length > 1)
+                {
+                    this.VersionPrefix = this.Lines[1].ItemSpec;
+                }
+
+                if (this.Lines.Length > 2)
+                {
+                    this.VersionSuffix = this.Lines[2].ItemSpec;
+                }
+
+                if (this.Lines.Length > 3)
+                {
+                    this.RepositoryCommit = this.Lines[3].ItemSpec;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/SemanticVersioning.MSBuild/WriteVersionToFile.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WriteVersionToFile.cs" company="Mondo">
+// Copyright (c) Mondo. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Mondo.SemanticVersioning
+{
+    using Microsoft.Build.Utilities;
+
+    /// <summary>
+    /// Writes the version to a file.
+    /// </summary>
+    public sealed class WriteVersionToFile : Microsoft.Build.Tasks.WriteLinesToFile
+    {
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        public string? Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version prefix.
+        /// </summary>
+        public string? VersionPrefix { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version suffix.
+        /// </summary>
+        public string? VersionSuffix { get; set; }
+
+        /// <summary>
+        /// Gets or sets the repository commit.
+        /// </summary>
+        public string? RepositoryCommit { get; set; }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            this.Lines = new Microsoft.Build.Framework.ITaskItem[]
+            {
+                new TaskItem(this.Version),
+                new TaskItem(this.VersionPrefix),
+                new TaskItem(this.VersionSuffix),
+                new TaskItem(this.RepositoryCommit),
+            };
+
+            return base.Execute();
+        }
+    }
+}
