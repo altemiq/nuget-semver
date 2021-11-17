306b105bb8d84b97407b3b0e979471544f60f041
Ross King <ross.king@ausnetservices.com.au>
2021-11-17 14:04:22 +1100

    fix: issue with getting enabled sources
    this was not getting the credentials before

--- src/SemanticVersioning.Core/NuGetInstaller.cs
+++ src/SemanticVersioning.Core/NuGetInstaller.cs
@@ -491,7 +491,7 @@ public static class NuGetInstaller
             .Select(source => GetFromMachineSources(source, enabledSources) ?? Repository.Factory.GetCoreV3(source)).ToArray();
         return repositories?.Length > 0
             ? repositories
-            : enabledSources.Select(packageSource => Repository.Factory.GetCoreV3(packageSource.Source)).ToArray();
+            : enabledSources.Select(packageSource => Repository.Factory.GetCoreV3(packageSource)).ToArray();
 
         static PackageSource ResolveSource(IEnumerable<PackageSource> availableSources, string source)
         {
