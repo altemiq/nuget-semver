9cefe5f895dd1ef754ff9b1549b0455bee1ceec4
Ross King <ross.king@ausnetservices.com.au>
2022-01-28 09:04:32 +1100

    feat: allow for compatible frameworks
    this caused an issue when appending the platform version in the nuget

--- src/SemanticVersioning.MSBuild/MSBuildApplication.cs
+++ src/SemanticVersioning.MSBuild/MSBuildApplication.cs
@@ -147,16 +147,25 @@ public static class MSBuildApplication
             var fullPackageOutputPath = TrimEndingDirectorySeparator(Path.Combine(projectDirectory, outputPath.Replace('\\', Path.DirectorySeparatorChar)));
 
             // check the frameworks
-            var currentFrameworks = Directory.EnumerateDirectories(fullPackageOutputPath).Select(Path.GetFileName).ToArray();
-            var previousFrameworks = Directory.EnumerateDirectories(installedBuildOutputTargetFolder).Select(Path.GetFileName).ToArray();
-            var frameworks = currentFrameworks.Intersect(previousFrameworks, StringComparer.OrdinalIgnoreCase);
+            var currentFrameworks = Directory
+                .EnumerateDirectories(fullPackageOutputPath)
+                .Select(Path.GetFileName)
+                .Select(NuGet.Frameworks.NuGetFramework.Parse)
+                .ToArray();
+            var previousFrameworks = Directory
+                .EnumerateDirectories(installedBuildOutputTargetFolder)
+                .Select(Path.GetFileName)
+                .Select(NuGet.Frameworks.NuGetFramework.Parse)
+                .ToArray();
+            var comparer = new NuGetFrameworkComparer();
+            var frameworks = currentFrameworks.Join(previousFrameworks, f => f, f => f, (current, previous) => (Current: current, Previous: previous), comparer);
             IList<NuGet.Versioning.SemanticVersion> previousVersions = previousPackages.Select<NuGet.Packaging.Core.PackageIdentity, NuGet.Versioning.SemanticVersion>(package => package.Version).ToList();
-            if (previousFrameworks.Except(currentFrameworks, StringComparer.OrdinalIgnoreCase).Any())
+            if (previousFrameworks.Except(currentFrameworks, comparer).Any())
             {
                 // we have removed frameworks, this is a breaking change
                 calculatedVersion = NuGetVersion.CalculateVersion(SemanticVersionChange.Major, previousVersions, getVersionSuffix(default), increment);
             }
-            else if (currentFrameworks.Except(previousFrameworks, StringComparer.OrdinalIgnoreCase).Any())
+            else if (currentFrameworks.Except(previousFrameworks, comparer).Any())
             {
                 // we have added frameworks, this is a feature change
                 calculatedVersion = NuGetVersion.CalculateVersion(SemanticVersionChange.Minor, previousVersions, getVersionSuffix(default), increment);
@@ -168,25 +177,30 @@ public static class MSBuildApplication
 #else
             const SearchOption options = SearchOption.TopDirectoryOnly;
 #endif
-            foreach (var currentAssembly in frameworks.SelectMany(framework => Directory.EnumerateFiles(Path.Combine(fullPackageOutputPath, framework ?? string.Empty), searchPattern, options)))
+            foreach (var (currentFramework, previousFramework) in frameworks)
             {
-                var oldAssembly = currentAssembly
-#if NETSTANDARD2_1_OR_GREATER
-                    .Replace(fullPackageOutputPath, installedBuildOutputTargetFolder, StringComparison.CurrentCulture);
-#else
-                    .Replace(fullPackageOutputPath, installedBuildOutputTargetFolder);
-#endif
-                var differences = LibraryComparison.DetectChanges(oldAssembly, currentAssembly);
-                var resultsType = File.Exists(oldAssembly)
-                    ? LibraryComparison.GetMinimumAcceptableChange(differences)
-                    : SemanticVersionChange.Major;
-                var version = NuGetVersion.CalculateVersion(resultsType == SemanticVersionChange.None ? SemanticVersionChange.Patch : resultsType, previousVersions, getVersionSuffix(default), increment);
-                if (version is not null && differences is not null)
+                foreach (var currentAssembly in Directory.EnumerateFiles(Path.Combine(fullPackageOutputPath, currentFramework.GetShortFolderName()), searchPattern, options))
                 {
-                    results.Add(new(version, differences));
-                }
+                    var previousAssembly = currentAssembly
+#if NETSTANDARD2_1_OR_GREATER
+                        .Replace(fullPackageOutputPath, installedBuildOutputTargetFolder, StringComparison.CurrentCulture)
+                        .Replace(currentFramework.GetShortFolderName(), previousFramework.GetShortFolderName(), StringComparison.CurrentCulture);
+#else
+                        .Replace(fullPackageOutputPath, installedBuildOutputTargetFolder)
+                        .Replace(currentFramework.GetShortFolderName(), previousFramework.GetShortFolderName());
+#endif
+                    var differences = LibraryComparison.DetectChanges(previousAssembly, currentAssembly);
+                    var resultsType = File.Exists(previousAssembly)
+                        ? LibraryComparison.GetMinimumAcceptableChange(differences)
+                        : SemanticVersionChange.Major;
+                    var version = NuGetVersion.CalculateVersion(resultsType == SemanticVersionChange.None ? SemanticVersionChange.Patch : resultsType, previousVersions, getVersionSuffix(default), increment);
+                    if (version is not null && differences is not null)
+                    {
+                        results.Add(new(version, differences));
+                    }
 
-                calculatedVersion = calculatedVersion.Max(version);
+                    calculatedVersion = calculatedVersion.Max(version);
+                }
             }
 
             Directory.Delete(installDir, recursive: true);
@@ -234,6 +248,45 @@ public static class MSBuildApplication
         static IEnumerable<T> CreateEnumerable<T>(T value)
         {
             yield return value;
+        }
+    }
+
+    private sealed class NuGetFrameworkComparer : IEqualityComparer<NuGet.Frameworks.NuGetFramework>
+    {
+        private readonly NuGet.Frameworks.DefaultCompatibilityProvider provider = new();
+
+        public bool Equals(NuGet.Frameworks.NuGetFramework x, NuGet.Frameworks.NuGetFramework y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (x is null || y is null)
+            {
+                return false;
+            }
+
+            if (x.Version == y.Version
+                && StringComparer.OrdinalIgnoreCase.Equals(x.Framework, y.Framework)
+                && StringComparer.OrdinalIgnoreCase.Equals(x.Profile, y.Profile)
+                && !x.IsUnsupported)
+            {
+                if (StringComparer.OrdinalIgnoreCase.Equals(x.Platform, y.Platform)
+                    && x.PlatformVersion == y.PlatformVersion)
+                {
+                    return true;
+                }
+
+                return this.provider.IsCompatible(x, y) || this.provider.IsCompatible(y, x);
+            }
+
+            return false;
+        }
+
+        public int GetHashCode(NuGet.Frameworks.NuGetFramework obj)
+        {
+            return obj is null ? 0 : HashCode.Combine(obj.Framework, obj.Version, obj.Profile);
         }
     }
 }
