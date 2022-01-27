0ed6bbe26679494aa8a95b40ce67799344a1d675
Ross King <ross.king@ausnetservices.com.au>
2022-01-28 09:04:32 +1100

    feat: allow for compatible frameworks
    this caused an issue when appending the platform version in the nuget

--- src/SemanticVersioning.Core/SemanticVersionIncrement.cs
+++ src/SemanticVersioning.Core/SemanticVersionIncrement.cs
@@ -1,10 +1,10 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="SemanticVersionIncrement.cs" company="Mondo">
-// Copyright (c) Mondo. All rights reserved.
+// <copyright file="SemanticVersionIncrement.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Mondo.SemanticVersioning;
+namespace Altemiq.SemanticVersioning;
 
 /// <summary>
 /// The <see cref="NuGet.Versioning.SemanticVersion"/> increment location.
